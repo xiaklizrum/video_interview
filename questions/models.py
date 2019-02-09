d5255c1ebc3e2c8bf42442159bd5d787770a44db
Oleg Shtanko <xiaklizrum@gmail.com>
2019-02-09 18:29:53 +1000

    Add models

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+
+class VideoInterview(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название', null=True)
+    job_seekers = models.ManyToManyField(
+        'JobSeeker', verbose_name='Соискатели', related_name='video_interview'
+    )
+    questions = models.ManyToManyField(
+        'Question', verbose_name='Вопросы', related_name='video_interview'
+    )
+
+    class Meta:
+        verbose_name = 'Видео-интервью'
+        verbose_name_plural = 'Видео-интервью'
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    title = models.CharField(
+        max_length=50, null=False, verbose_name='Название'
+    )
+    text = models.TextField(null=False, verbose_name='Содержание вопроса')
+    seconds_to_answer = models.IntegerField(
+        null=False, default=60, verbose_name='Время на ответ (в секундах)'
+    )
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+    def __str__(self):
+        return self.title
+
+
+class JobSeeker(models.Model):
+    name = models.CharField(max_length=50, null=False, verbose_name='Имя')
+
+    class Meta:
+        verbose_name = 'Соискатель'
+        verbose_name_plural = 'Соискатели'
+
+    def __str__(self):
+        return self.name
